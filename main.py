c8b330ffb43edb69c8a4c0447e1ad6ecb7088f36
Gilang Pratama <pratamapriadi96@gmail.com>
2021-10-26 00:01:41 +0700

    :zap: update 002

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {"kangketik": {"age": 25, "gender": "male"},
+         "gilang": {"age": 25, "gender": "male"}}
+
 
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+    def get(self, name):
+        return names[name]
 
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if(__name__) == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-resonse = requests.get(BASE + "helloworld")
+resonse = requests.get(BASE + "helloworld/gilang")
 
 print(resonse.json())
