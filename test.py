36064c457ce97ffa41d11e3bde1f37c62cfc496e
Gilang Pratama <pratamapriadi96@gmail.com>
2021-10-26 02:05:50 +0700

    :zap: update 003

--- test.py
+++ test.py
@@ -3,18 +3,21 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 data = [
-    {"nama": "Gilang", "usia": 25},
-    {"nama": "Kangketik", "usia": 25},
-    {"nama": "Random Guy", "usia": 25},
+    {"nama": "Gilang", "usia": 20},
+    {"nama": "Kangketik", "usia": 21},
+    {"nama": "Random Guy", "usia": 30},
 ]
 
 for i in range(len(data)):
-    resonse = requests.put(BASE + "karyawan/"+str(i), data[i])
+    resonse = requests.patch(BASE + "karyawan/" + str(i), data[i])
     print(resonse.json())
 
 input()
-response = requests.delete(BASE + "karyawan/0")
-print(resonse.json())
+resonse = requests.get(BASE + "karyawan/")
+print(resonse)
 input()
 resonse = requests.get(BASE + "karyawan/1")
+print(resonse.json())
+input()
+response = requests.delete(BASE + "karyawan/0")
 print(resonse.json())
