b779162146ae7513f244a8def6e9b56c7df1a216
Gilang Pratama <pratamapriadi96@gmail.com>
2021-10-26 18:37:26 +0700

    :zap: update bugs

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,17 @@ from flask import Blueprint
 from .main.controller.user_controller import api as user_ns
 from .main.controller.auth_controller import api as auth_ns
 
-blueprint = Blueprint('api', __name__)
-authorizations = {
-    'apikey': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization'
-    }
-}
+blueprint = Blueprint("api", __name__)
+authorizations = {"apikey": {"type": "apiKey", "in": "header", "name": "Authorization"}}
 
 api = Api(
     blueprint,
-    title='FLASK RESTPLUS(RESTX) API BOILER-PLATE WITH JWT',
-    version='1.0',
-    description='a boilerplate for flask restplus (restx) web service',
+    title="FLASK RESTful API",
+    version="1.0",
+    description="a boilerplate for flask restplus (restx) web service",
     authorizations=authorizations,
-    security='apikey'
+    security="apikey",
 )
 
-api.add_namespace(user_ns, path='/user')
+api.add_namespace(user_ns, path="/user")
 api.add_namespace(auth_ns)
